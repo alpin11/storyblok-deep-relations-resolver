9f783e6e8faf18e6eab29f185dc4add493b20b4c
David Höck <d.hoeck@alpin11.com>
2023-03-09 10:36:25 +0100

    ~ fix paths

--- dist/@alpin11/storyblok-deep-relations-resolver.mjs
+++ dist/@alpin11/storyblok-deep-relations-resolver.mjs
@@ -1,37 +1,37 @@
-import p, { forwardRef as P } from "react";
-var d = Object.defineProperty, l = Object.getOwnPropertySymbols, u = Object.prototype.hasOwnProperty, y = Object.prototype.propertyIsEnumerable, f = (o, e, r) => e in o ? d(o, e, { enumerable: !0, configurable: !0, writable: !0, value: r }) : o[e] = r, g = (o, e) => {
-  for (var r in e || (e = {}))
-    u.call(e, r) && f(o, r, e[r]);
+import p, { forwardRef as d } from "react";
+var P = Object.defineProperty, l = Object.getOwnPropertySymbols, u = Object.prototype.hasOwnProperty, y = Object.prototype.propertyIsEnumerable, f = (o, e, t) => e in o ? P(o, e, { enumerable: !0, configurable: !0, writable: !0, value: t }) : o[e] = t, g = (o, e) => {
+  for (var t in e || (e = {}))
+    u.call(e, t) && f(o, t, e[t]);
   if (l)
-    for (var r of l(e))
-      y.call(e, r) && f(o, r, e[r]);
+    for (var t of l(e))
+      y.call(e, t) && f(o, t, e[t]);
   return o;
 }, O = (o, e) => {
-  var r = {};
-  for (var t in o)
-    u.call(o, t) && e.indexOf(t) < 0 && (r[t] = o[t]);
+  var t = {};
+  for (var r in o)
+    u.call(o, r) && e.indexOf(r) < 0 && (t[r] = o[r]);
   if (o != null && l)
-    for (var t of l(o))
-      e.indexOf(t) < 0 && y.call(o, t) && (r[t] = o[t]);
-  return r;
+    for (var r of l(o))
+      e.indexOf(r) < 0 && y.call(o, r) && (t[r] = o[r]);
+  return t;
 };
-const k = P((o, e) => {
-  var r = o, { blok: t } = r, a = O(r, ["blok"]);
-  if (!t)
+const k = d((o, e) => {
+  var t = o, { blok: r } = t, a = O(t, ["blok"]);
+  if (!r)
     return console.error("Please provide a 'blok' property to the StoryblokComponent"), /* @__PURE__ */ p.createElement("div", null, "Please provide a blok property to the StoryblokComponent");
-  const n = C(t.component);
+  const n = C(r.component);
   return n ? /* @__PURE__ */ p.createElement(n, g({
     ref: e,
-    blok: t
+    blok: r
   }, a)) : /* @__PURE__ */ p.createElement("div", null);
 });
 k.displayName = "StoryblokComponent";
 let w = null;
 const h = () => (console.error("You can't use getStoryblokApi if you're not loading apiPlugin."), w);
 let c = {};
-const C = (o) => c[o] ? c[o] : (console.error(`Component ${o} doesn't exist.`), !1), S = async (o, e, r) => {
-  for (const t of e) {
-    const [a, n] = t.split("."), v = b(a, o);
+const C = (o) => c[o] ? c[o] : (console.error(`Component ${o} doesn't exist.`), !1), S = async (o, e, t) => {
+  for (const r of e) {
+    const [a, n] = r.split("."), v = b(a, o);
     for (const i of v) {
       const s = i[n];
       Array.isArray(s) ? i[n] = await Promise.all(s.map(async (_) => await m(_))) : typeof s == "string" ? i[n] = await m(s) : typeof s == "object" && S(s, e);
@@ -39,16 +39,16 @@ const C = (o) => c[o] ? c[o] : (console.error(`Component ${o} doesn't exist.`), 
   }
   return o;
 }, b = (o, e) => {
-  const r = [];
-  return e = e ?? {}, e.component === o && r.push(e), Object.values(e).forEach((t) => {
-    typeof t == "object" && r.push(...b(o, t));
-  }), r;
+  const t = [];
+  return e = e ?? {}, e.component === o && t.push(e), Object.values(e).forEach((r) => {
+    typeof r == "object" && t.push(...b(o, r));
+  }), t;
 }, m = async (o, e) => {
-  const r = h(), { data: t } = await r.get("cdn/stories", {
+  const t = h(), { data: r } = await t.get("cdn/stories", {
     ...e,
     by_uuids: o
   });
-  return t.stories.length > 0 ? t.stories[0] : o;
+  return r.stories.length > 0 ? r.stories[0] : o;
 };
 export {
   S as resolveRelationsDeep
